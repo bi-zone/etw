1cab4b3f467808ab81cb43dbd92a805e1a14ca89
Leandro Costantino <lcostantino@gmail.com>
2022-07-12 20:49:45 -0300

    Unsafe Ptr

--- event.go
+++ event.go
@@ -518,6 +518,6 @@ func createUTF16String(ptr uintptr, len int) string {
 	if len == 0 {
 		return ""
 	}
-	bytes := unsafe.Slice(ptr, len)
+	bytes := unsafe.Slice(unsafe.Pointer(ptr), len)
 	return windows.UTF16ToString(bytes)
 }
